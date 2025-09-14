d42c2eb63f1ca679bc07ade15760a7d3d453e845
cyucccx <yuyuchen2005@gmail.com>
2025-09-14 12:47:11 +0800

    Delete MVP

--- preload/preload.js
+++
@@ -1,9 +0,0 @@
-
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getDisplays: () => ipcRenderer.invoke('get-displays'),
-  moveWindow: (x, y) => ipcRenderer.invoke('move-window', { x, y }),
-  setMouseThrough: (ignore) => ipcRenderer.invoke('toggle-mouse-through', ignore),
-  toggleChat: () => ipcRenderer.invoke('toggle-chat')
-});
